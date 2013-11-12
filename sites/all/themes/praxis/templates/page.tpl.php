7be3665c07acfbbadf28bc120016669af9fdcb10
iryna.gychka <iryna.gychka@masterofcode.com>
2013-11-12 18:28:44 +0200

    news

--- sites/all/themes/praxis/templates/page.tpl.php
+++ sites/all/themes/praxis/templates/page.tpl.php
@@ -149,74 +149,78 @@
 
 
 
+        <div class="content-pages-wrapper">
+            <div class="main_container_inner">
+        
+                <?php if ($messages): ?>
+                    <div id="messages"><div class="section clearfix">
+                            <?php print $messages; ?>
+                        </div></div> <!-- /.section, /#messages -->
+                <?php endif; ?>
 
-        <?php if ($messages): ?>
-            <div id="messages"><div class="section clearfix">
-                    <?php print $messages; ?>
-                </div></div> <!-- /.section, /#messages -->
-        <?php endif; ?>
+                <?php if ($page['featured']): ?>
+                    <div id="featured"><div class="section clearfix">
+                            <?php print render($page['featured']); ?>
+                        </div></div> <!-- /.section, /#featured -->
+                <?php endif; ?>
+                
+                <?php if ($page['sidebar_first']): ?>
+                    <div id="sidebar-first" class="column sidebar-right"><div class="section">
+                            <?php print render($page['sidebar_first']); ?>
+                        </div></div> <!-- /.section, /#sidebar-first -->
+                <?php endif; ?>
 
-        <?php if ($page['featured']): ?>
-            <div id="featured"><div class="section clearfix">
-                    <?php print render($page['featured']); ?>
-                </div></div> <!-- /.section, /#featured -->
-        <?php endif; ?>
+                <div id="content" class="column"><div class="section">
+                        <div id="main" class="">
+                                
+                                <?php if ($breadcrumb): ?>
+                                    <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+                                <?php endif; ?>
 
-        <?php if ($page['sidebar_first']): ?>
-            <div id="sidebar-first" class="column sidebar"><div class="section">
-                    <?php print render($page['sidebar_first']); ?>
-                </div></div> <!-- /.section, /#sidebar-first -->
-        <?php endif; ?>
+                                <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+                                <div class="body-inner">
+                                    <a id="main-content"></a>
+                                    <!-- <?php print render($title_prefix); ?>
+                                    <?php if ($title): ?>
+                                        <h1 class="title" id="page-title">
+                                            <?php print $title; ?>
+                                        </h1>
+                                    <?php endif; ?>-->
+                                    <?php print render($title_suffix); ?>
+                                    <?php if ($tabs): ?>
+                                        <div class="tabs">
+                                            <?php print render($tabs); ?>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php print render($page['help']); ?>
+                                    <?php if ($action_links): ?>
+                                        <ul class="action-links">
+                                            <?php print render($action_links); ?>
+                                        </ul>
+                                    <?php endif; ?>
+                                    <?php print render($page['content']); ?>
+                                    <?php print $feed_icons; ?>
 
-        <div id="content" class="column"><div class="section">
-                <div id="main-wrapper" class="clearfix"><div class="main-wrap-bg"></div><div id="main" class="clearfix">
-                        
-                        <?php if ($breadcrumb): ?>
-                            <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-                        <?php endif; ?>
+                                </div></div> <!-- /.section, /#content -->
 
-                        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-                        <div class="body-inner">
-                            <a id="main-content"></a>
-                            <?php print render($title_prefix); ?>
-                            <?php if ($title): ?>
-                                <h1 class="title" id="page-title">
-                                    <?php print $title; ?>
-                                </h1>
+                            <?php if ($page['sidebar_second']): ?>
+                                <div id="sidebar-second" class="column sidebar"><div class="section">
+                                        <?php print render($page['sidebar_second']); ?>
+                                    </div></div> <!-- /.section, /#sidebar-second -->
                             <?php endif; ?>
-                            <?php print render($title_suffix); ?>
-                            <?php if ($tabs): ?>
-                                <div class="tabs">
-                                    <?php print render($tabs); ?>
-                                </div>
-                            <?php endif; ?>
-                            <?php print render($page['help']); ?>
-                            <?php if ($action_links): ?>
-                                <ul class="action-links">
-                                    <?php print render($action_links); ?>
-                                </ul>
-                            <?php endif; ?>
-                            <?php print render($page['content']); ?>
-                            <?php print $feed_icons; ?>
+                        </div>
 
-                        </div></div> <!-- /.section, /#content -->
+                    </div></div> <!-- /#main, /#main-wrapper -->
 
-                    <?php if ($page['sidebar_second']): ?>
-                        <div id="sidebar-second" class="column sidebar"><div class="section">
-                                <?php print render($page['sidebar_second']); ?>
-                            </div></div> <!-- /.section, /#sidebar-second -->
-                    <?php endif; ?>
-                </div>
-
-            </div></div> <!-- /#main, /#main-wrapper -->
-
-        <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
-            <div id="triptych-wrapper"><div id="triptych" class="clearfix">
-                    <?php print render($page['triptych_first']); ?>
-                    <?php print render($page['triptych_middle']); ?>
-                    <?php print render($page['triptych_last']); ?>
-                </div></div> <!-- /#triptych, /#triptych-wrapper -->
-        <?php endif; ?>
+                <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
+                    <div id="triptych-wrapper"><div id="triptych" class="clearfix">
+                            <?php print render($page['triptych_first']); ?>
+                            <?php print render($page['triptych_middle']); ?>
+                            <?php print render($page['triptych_last']); ?>
+                        </div></div> <!-- /#triptych, /#triptych-wrapper -->
+                <?php endif; ?>
+            </div>    
+        </div>
 
         <div id="footer-wrapper">
 
@@ -240,3 +244,4 @@
         </div> <!-- /.section, /#footer-wrapper -->
 
     </div></div> <!-- /#page, /#page-wrapper -->
+
