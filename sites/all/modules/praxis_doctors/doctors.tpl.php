e13f28d6f64e435290e756c56ac978e329f6059f
Sergey Gychka <sergey.gychka@gmail.com>
2014-05-27 23:33:06 +0300

    fix sitemap

--- sites/all/modules/praxis_doctors/doctors.tpl.php
+++ sites/all/modules/praxis_doctors/doctors.tpl.php
@@ -5,7 +5,7 @@
             <ul>
                 <?php foreach($slide as $node): ?>
                     <li class="doctor-thumbnail">
-                        <a href="<?php if ($lang != 'en'): ?>/<?php print $lang; ?><?php endif ?>/team/doctors/<?php print $node->nid ?>">
+                        <a href="<?php if ($lang != 'de'): ?>/<?php print $lang; ?><?php endif ?>/team/doctors/<?php print $node->nid ?>">
                             <?php
                                 $image = array(
                                     'style_name' => 'home_doctor',
